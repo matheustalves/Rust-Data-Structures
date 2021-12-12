dac2019bb392bb18a23a8c6df9834e9d43ce3f04
Matheus Teixeira <matheustalves@outlook.com>
2021-12-12 20:39:48 -0300

    added new methods

--- singly_list/src/main.rs
+++ singly_list/src/main.rs
@@ -1,6 +1,6 @@
 use std::mem;
 
-#[derive(Debug)]
+// #[derive(Debug)]
 struct Cell<T> {
     data: T,
     next: Option<Box<Cell<T>>>,
@@ -8,29 +8,57 @@ struct Cell<T> {
 
 pub struct SinglyList<T> {
     first: Option<Box<Cell<T>>>,
+    length: u32,
 }
 
 impl<T> SinglyList<T> {
     pub fn new() -> Self{
         SinglyList{
             first: None,
+            length: 0,
         }
     }
 
-    pub fn insert_start(&mut self, data: T) {
-        let old_first = mem::replace(&mut self.first, None);
+    pub fn len(&self) -> u32 {
+        self.length
+    }
+
+    pub fn push(&mut self, data: T) {
+        let old_first = self.first.take();
         let cell = Cell {
             data,
             next: old_first,
         };
         self.first = Some(Box::new(cell));
+        self.length += 1;
     }
+
+    // pub fn get(&self, index: u32) -> Option<&T>{
+    //     if index < self.length{
+    //         let current_elem = &self.first;
+    //         match &self.first{
+    //             Some(elem) => Some(&elem.data),
+    //             None => None,
+    //         }
+    //     }else{
+
+    //     }
+    // }
+
+    // pub fn insert_at(&mut self, data: T, index: u32){
+
+    // }
 }
 
 fn main() {
-    // let mut list = new_list(42);
     let mut list = SinglyList::new();
-    list.insert_start(79);
-    list.insert_start(89);
-    print!("{:?}",list.first);
+    list.push(42);
+    list.push(66);
+
+    // match list.get(0){
+    //     Some(elem) => println!("element: {}", elem),
+    //     None => println!("No element."),
+    // };
+
+    println!("{}", list.len());
 }
